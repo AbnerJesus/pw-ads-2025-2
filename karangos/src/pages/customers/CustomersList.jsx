2a67548c38adf424ff8a5c567de6184d3f10f5b4
Abner Jesus <abnerjesus20@hotmail.com>
2025-11-13 11:09:19 -0300

    (13/11) Implementação do Data Grid na listagem de clientes

--- karangos/src/pages/customers/CustomersList.jsx
+++ karangos/src/pages/customers/CustomersList.jsx
@@ -48,19 +48,6 @@ export default function CustomersList() {
  ];
 
 
-
- const rows = [
-   { id: 1, lastName: 'Snow', firstName: 'Jon', age: 14 },
-   { id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 31 },
-   { id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 31 },
-   { id: 4, lastName: 'Stark', firstName: 'Arya', age: 11 },
-   { id: 5, lastName: 'Targaryen', firstName: 'Daenerys', age: null },
-   { id: 6, lastName: 'Melisandre', firstName: null, age: 150 },
-   { id: 7, lastName: 'Clifford', firstName: 'Ferrara', age: 44 },
-   { id: 8, lastName: 'Frances', firstName: 'Rossini', age: 36 },
-   { id: 9, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
- ];
-
  const [customers, setCustomers] = React.useState([])
 
 
@@ -107,9 +94,6 @@ return <>
        disableRowSelectionOnClick
      />
    </Box>
-   <div>
-     { JSON.stringify(customers) }
-   </div>
  </>
 }
 
